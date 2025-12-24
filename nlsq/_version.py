2518e853606eb741157f938dbcd685d77c6a04f2
Wei Chen <wchen@anl.gov>
2025-12-23 21:00:46 -0600

    Dependency Verison Updates

--- nlsq/_version.py
+++ nlsq/_version.py
@@ -15,7 +15,7 @@ if TYPE_CHECKING:
     from typing import Tuple
     from typing import Union
 
-    VERSION_TUPLE = tuple[Union[int, str], ...]
+    VERSION_TUPLE = Tuple[Union[int, str], ...]
     COMMIT_ID = Union[str, None]
 else:
     VERSION_TUPLE = object
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.3.8.post0'
-__version_tuple__ = version_tuple = (0, 3, 8, 'post0')
+__version__ = version = '0.3.7.post15'
+__version_tuple__ = version_tuple = (0, 3, 7, 'post15')
 
-__commit_id__ = commit_id = 'ge634637f1'
+__commit_id__ = commit_id = 'g76e40756b'
